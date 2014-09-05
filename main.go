13a19d3d6575a377f7f43ffcd02fe1e9fec9e56c
admin <admin@macmini.local>
2014-09-05 10:26:51 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+
+	"github.com/go-martini/martini"
+)
+
+var port = flag.Int("p", 9527, "port to listen")
+var secret = flag.String("s", "", "github secret")
+var script = flag.String("e", "", "script to execute")
+
+func index(p martini.Params) {
+	log.Println(p)
+	out, err := exec.Command(*script).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(out)
+}
+
+func main() {
+	flag.Parse()
+	if *script == "" || *secret == "" {
+		flag.PrintDefaults()
+		return
+	}
+	m := martini.Classic()
+	m.Post("/", index)
+	addr := fmt.Sprintf(":%d", *port)
+	log.Fatal(http.ListenAndServe(addr, m))
+}
